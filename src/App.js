c589a5b6b902166a51d0e1154ee50dd67dc5941c
Febrianto <febrianto.bekasi@gmail.com>
2022-09-21 21:42:29 +0700

    create multiple reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createStore, bindActionCreators } from "redux";
+
+// create action
+const orderCake = () => {
+  return {
+    type: "ORDERED_CAKE",
+    payload: 1,
+  };
+};
+
+const restockCake = (sum) => {
+  return {
+    type: "RESTOCK_CAKE",
+    payload: sum,
+  };
+};
+
+const orderIceCream = () => {
+  return {
+    type: "ORDER_ICECREAM",
+    payload: 1,
+  };
+};
+
+const restockIceCream = (sum) => {
+  return {
+    type: "RESTOCK_ICECREAM",
+    payload: sum,
+  };
+};
+
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams: 0,
+// };
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+
+const initialIceCreamState = {
+  numOfIceCreams: 5,
+};
+
+// create reducer
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case "ORDERED_CAKE":
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    case "RESTOCK_CAKE":
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case "ORDER_ICECREAM":
+      return {
+        numOfIceCreams: state.numOfIceCream - 1,
+      };
+    case "RESTOCK_ICECREAM":
+      return {
+        numOfIceCreams: state.numOfIceCream + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+// create store
+const store = createStore(cakeReducer);
+console.log("Initial State : ", store.getState());
+
+store.subscribe(() => console.log("Updated State : ", store.getState())); // selalu di jalankan ketika dispatch(action())
+store.dispatch(orderCake()); // setiap kali dispatch(action()) maka state akan terupdate sesuai dengan logic yang ada di reducer
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(restockCake(5));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
